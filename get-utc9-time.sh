68239af7304f9dfabf50ba143fd70b1033db33d6
LI_CHENG <90380376+BILILICHENG@users.noreply.github.com>
2024-11-07 19:01:35 +0900

    Update get-utc9-time.sh

--- get-utc9-time.sh
+++ get-utc9-time.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
- 
-# 获取当前UTC时间，并转换为UTC+9时间
-utc_time=$(date -u +"%Y-%m-%d %H:%M:%S")
-utc9_time=$(TZ=Asia/Tokyo date -d "$utc_time" +"%Y-%m-%d %H:%M:%S")
- 
+
+# 获取当前 UTC+9 时间
+utc9_time=$(TZ=Asia/Tokyo date +"%Y-%m-%d %H:%M:%S")
+
 # 将时间写入文件
 echo "$utc9_time" > utc9-time.txt
